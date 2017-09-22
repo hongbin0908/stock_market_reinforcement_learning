d3b35723be40719fb25d33752615d1a26e7b0c83
Bin Hong <abin@192.168.0.105>
2017-09-22 15:57:25 +0800

    xxxx

---
+++ market_pg_paper_ploat.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+local_path = os.path.dirname(__file__)
+root = os.path.join(local_path, '..')
+sys.path.append(root)
+
+if __name__ == "__main__":
+
+    argi = 1
+    code = sys.argv[argi]; argi += 1
+    start = sys.argv[argi]; argi += 1
+    end = sys.argv[argi]; argi += 1
+
+    contents = os.walk(local_path)
+    for root, dirs, files in contents:
+        for f in files:
+            print(os.path.join(root, f))
