c7cb6fab1b3b5a378acaf7b89a09f1a97a75c4b9
Bin Hong <abin@192.168.0.105>
2017-09-26 00:32:17 +0800

    xxxx

--- market_pg_train_b.py
+++ market_pg_train_b.py
@@ -22,7 +22,7 @@ for year in range(2000, 2018):
     train_end = train_end_obj.strftime('%Y-%m-%d')
     train_start_obj = train_end_obj - timedelta(days = 365)
     train_start = train_start_obj.strftime('%Y-%m-%d')
-    cmd_str = 'python3 market_pg_train.py %s %s %s %s %s 1' % (codeListFilename, train_start, train_end, test_start, test_end)
+    cmd_str = 'python3 market_pg_train.py %s %s %s %s %s 100' % (codeListFilename, train_start, train_end, test_start, test_end)
     e = pool.apply_async(util.run_cmd, (cmd_str,))
     res.append(e)
 for e in res:
--- util.py
+++ util.py
@@ -10,5 +10,16 @@ sys.path.append(root)
 def run_cmd(cmd_str):
     print(cmd_str)
     p = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    out, err = p.communicate()
-    return (out, err)
+    while True:
+        out = p.stdout.read(1)
+        if out == '' and p.poll() != None:
+            break
+        if out != '':
+            sys.stdout.write(out)
+            sys.stdout.flush()
+        err = p.stderr.read(1)
+        if err == '' and p.poll() != None:
+            break
+        if err != '':
+            sys.stderr.write(out)
+            sys.stderr.flush()
