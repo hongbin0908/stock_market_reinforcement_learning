1f9762959e9d0e693f9e570c466502e0947d4b00
Bin Hong <abin@192.168.0.105>
2017-09-26 00:39:43 +0800

    xxxx

--- util.py
+++ util.py
@@ -10,10 +10,5 @@ sys.path.append(root)
 def run_cmd(cmd_str):
     print(cmd_str)
     p = subprocess.Popen(cmd_str, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    while True:
-        out = p.stdout.read(1)
-        if out == '' and p.poll() != None:
-            break
-        if out != '':
-            sys.stdout.write(out.decode('utf-8'))
-            sys.stdout.flush()
+    out, err = p.communicate()
+    return (out, err)
