a317da6afae8a91402eff56825f421852af71167
Bin Hong <hongbin@youzan.com>
2017-09-28 11:21:02 +0800

    xxx

--- market_pg_b.py
+++ market_pg_b.py
@@ -10,11 +10,7 @@ import util
 
 
 def train_and_paper(code_list, code):
-<<<<<<< HEAD
     pool = ThreadPool(1)
-=======
-    pool = ThreadPool(4)
->>>>>>> 6bd9016a16e79387a9d921b39b2e39e087e5db24
     res = []
     for year in range(2000, 2018):
         train_start, train_end, test_start, test_end = util.train_test_range(year)
